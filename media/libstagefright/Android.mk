75898c33546365dcc00e589fb26b42c4be6b6205
Alex Naidis <alex.naidis@linux.com>
2016-10-04 13:00:56 +0200

    media: never enable AV Experiments automatically
    * only if explicitly requested via TARGET_ENABLE_AV_EXPERIMENTS
    
    Change-Id: Idb06b4f8757ffda7c5b432cbad6ff8392377e64c
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -143,8 +143,8 @@ LOCAL_SHARED_LIBRARIES += \
 
 LOCAL_CFLAGS += -Wno-multichar -Werror -Wno-error=deprecated-declarations -Wall
 
-# enable experiments only in userdebug and eng builds
-ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
+# enable experiments only if requested
+ifeq ($(TARGET_ENABLE_AV_EXPERIMENTS),true)
 LOCAL_CFLAGS += -DENABLE_STAGEFRIGHT_EXPERIMENTS
 endif
 # DOLBY_START
